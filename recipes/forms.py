3c49d785f1fe3f81183b37fb6ec4800eb5f5a2d2
Michisu, Toshikazu <michisu@marici.co.jp>
2012-05-11 15:22:32 +0900

    fixed password reset

--- recipes/forms.py
+++ recipes/forms.py
@@ -182,40 +182,6 @@ class UserProfileForm(forms.ModelForm):
         self.fields['blog_url'].widget = _text_input(40)
 
 
-class PasswordResetForm(auth_forms.PasswordResetForm):
-
-    def save(self, domain_override=None,
-            email_template_name='registration/password_reset_email.html',
-            use_https=False, token_generator=default_token_generator):
-        '''
-        Generates a one-use only link for resetting password and sends to the
-        user
-        '''
-        for user in self.users_cache:
-            if not domain_override:
-                current_site = Site.objects.get_current()
-                site_name = current_site.name
-                domain = current_site.domain
-            else:
-                site_name = domain = domain_override
-            t = loader.get_template(email_template_name)
-            c = {
-                'email': user.email,
-                'domain': domain,
-                'site_name': site_name,
-                'uid': int_to_base36(user.id),
-                'user': user,
-                'token': token_generator.make_token(user),
-                'protocol': use_https and 'https' or 'http',
-            }
-            task = SendEmailTask(dict(subject=u'[%s] パスワードをリセット' % site_name,
-                                      body=t.render(Context(c)),
-                                      from_address=settings.EMAIL_FROM,
-                                      to_list=[user.email]))
-            get_taskqueue().send_task(task,
-                    queue_name=settings.QUEUENAME_EMAIL)
-
-
 class NewRecipeForm(forms.ModelForm):
     '''
     新規にレシピを作成するフォーム。
